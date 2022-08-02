bccdfde44b7209eee00502603b7c31560a0100f2
chensan6 <chenhong01@yxqiche.com>
2022-08-02 22:15:35 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/alieen/wiki/controller/TestController.java
+++ src/main/java/com/alieen/wiki/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.alieen.wiki.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
@@ -13,5 +15,12 @@ public class TestController {
     public String hello(){
 
         return "Hello Word!";
+    }
+
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+
+        return "Hello Word! Post."+ name;
     }
 }
