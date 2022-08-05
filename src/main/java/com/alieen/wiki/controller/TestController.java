2b5dbbd38735fdd363a1ede0b7757089ddce0a2a
chensan6 <chenhong01@yxqiche.com>
2022-08-05 17:54:20 +0800

    2-8 增加自定义配置项的示例

--- src/main/java/com/alieen/wiki/controller/TestController.java
+++ src/main/java/com/alieen/wiki/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.alieen.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
 
     @GetMapping("/hello")
 //    @PostMapping
@@ -14,7 +18,7 @@ public class TestController {
 //    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
 
-        return "Hello Word!";
+        return "Hello Word!"+testHello;
     }
 
 
